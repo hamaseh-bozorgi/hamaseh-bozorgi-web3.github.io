65592913c07aa6975b38bb61ab6f67c292a63769
hamaseh-bozorgi <hamase_bz@yahoo.com>
2023-03-10 05:37:17 -0800

    navigation

--- js/index.js
+++ js/index.js
@@ -8,12 +8,47 @@ $(document).ready(function () {
     $(".mobile-click").click(function () {
         $(".mobile-sub-menu-sm").toggleClass("custom-height");
     })
-    $(".gaming").mouseenter(function () {
-        $(".sub-gaming").removeClass("hidden");
+
+    $($(".mobile-titles")[0]).mouseenter(function () {
+        $(".w-h").addClass("hidden");
+        $(".Accessories").addClass("hidden");
+        $(".phones").removeClass("hidden");
     })
-    $(".gaming").mouseout(function () {
-        $(".sub-gaming").addClass("hidden");
+    $($(".mobile-titles")[1]).mouseenter(function () {
+        $(".phones").addClass("hidden");
+        $(".Accessories").addClass("hidden");
+        $(".w-h").removeClass("hidden");
     })
+    $($(".mobile-titles")[2]).mouseenter(function () {
+        $(".phones").addClass("hidden");
+        $(".w-h").addClass("hidden");
+        $(".Accessories").removeClass("hidden");
+    })
+
+
+    let activeImage = 0;
+    let img = document.querySelector(".slider-image");
+    let sliderRight = function () {
+        activeImage++;
+        if (activeImage == 6) {
+            activeImage = 0;
+        }
+        let imgWidth = img.clientWidth;
+        img.style.marginLeft = `-${activeImage * imgWidth}px`;
+    }
+    setInterval(sliderRight, 3000);
+    let sliderLeft = function () {
+        activeImage--;
+        if (activeImage == -1) {
+            activeImage = 5;
+        }
+        let imgWidth = img.clientWidth;
+        img.style.marginLeft = `-${activeImage * imgWidth}px`;
+    }
+    $(".right-btn").click(sliderRight);
+    $(".left-btn").click(sliderLeft);
+
+
     $($(".footer-titles")[0]).click(function () {
         $(".footer-sub-1").toggleClass("custom-height");
     })
@@ -23,25 +58,5 @@ $(document).ready(function () {
     $($(".footer-titles")[2]).click(function () {
         $(".footer-sub-3").toggleClass("custom-height");
     })
-    let activeImage=0;
-    let img=document.querySelector(".slider-image");
-    let sliderRight=function(){
-        activeImage++;
-        if(activeImage==6){
-            activeImage=0;
-        }
-        let imgWidth=img.clientWidth;
-        img.style.marginLeft=`-${activeImage *imgWidth}px`;
-    }
-    setInterval(sliderRight,3000);
-    let sliderLeft=function(){
-        activeImage--;
-        if(activeImage==-1){
-            activeImage=5;
-        }
-        let imgWidth=img.clientWidth;
-        img.style.marginLeft=`-${activeImage *imgWidth}px`;
-    }
-    $(".right-btn").click(sliderRight);
-    $(".left-btn").click(sliderLeft);
+
 });
